d440f91c143cebd87d61c5db2e53fb5aeb18bde0
ajf01 <ajf010@ucsd.edu>
2023-08-26 04:48:40 -0700

    React App Created. Default files removed.

---
+++ portfolio/src/components/NavBar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <img src="https://icon-library.com/icon/react-icon-29.html.html" alt="React Logo" className="Logo"></img>
+            <div className="desktopMenu">
+
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar

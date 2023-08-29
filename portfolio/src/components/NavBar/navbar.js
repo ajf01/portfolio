875210bcbef36d691bb398fad744aba647a02246
ajf01 <ajf010@ucsd.edu>
2023-08-28 18:46:10 -0700

    Added some additional style elements for beauty

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
         <nav className="navbar">
             <img src="https://miro.medium.com/v2/resize:fit:80/format:webp/1*y6C4nSvy2Woe0m7bWEn4BA.png" alt="Logo" className="Logo"></img>
             <div className="Menu">
-                <Link className="MenuItem">Home</Link>
-                <Link className="MenuItem">About</Link>
-                <Link className="MenuItem">Portfolio</Link>
-                <Link className="MenuItem">Clients</Link>
+                <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="MenuItem">Home</Link>
+                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">About</Link>
+                <Link activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Portfolio</Link>
+                <Link activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Clients</Link>
             </div>
-            <button className="ContactButton">
+            <button className="ContactButton" onClick={() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}>
                 <img src={require("../../assets/contact.png")} alt="Contact" className="ContactButtonImg"></img>
                 Contact Me
             </button>

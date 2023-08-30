c4fd63fdf00d5777f236fbb13b14613e5cfb81d1
ajf01 <ajf010@ucsd.edu>
2023-08-30 00:51:54 -0700

    Linked "Hire Me" button to contact section

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,7 +9,11 @@ const Intro = () => {
                 <span className="hello">Hello, </span>
                 <span className="introText">I'm <span className="myName">Anthony</span> <br/>Data Scientist</span>
                 <p className="introPara">I have worked on many coding questions and am proficient in data.</p>
-                <Link><button className="hireBtn">Hire Me</button></Link>
+                <Link>
+                    <button className="hireBtn" onClick={() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}>
+                        Hire Me
+                    </button>
+                </Link>
             </div>
             <img src="https://images2.alphacoders.com/475/475527.jpg" alt="MainPageImg" className="bgImg"></img>
         </section>

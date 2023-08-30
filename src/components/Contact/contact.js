93ccc9c4ad1f0bc01bfbd597116a50db539efe28
ajf01 <ajf010@ucsd.edu>
2023-08-29 19:22:49 -0700

    Finished all pages/elements. Host to github pages

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import './contact.css'
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm("serviceID", "templateID", form.current, "publicKey").then((result) => {
+            console.log(result.text);
+            e.target.reset()
+            alert('Email Sent!')
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
     return (
         <section id="contactPage">
             <div id="clients">
@@ -19,9 +31,9 @@ const Contact = () => {
             <div id="contact">
                 <h1 className="pageTitle">Contact Me</h1>
                 <span className="contactDesc">Hello</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your Name" />
-                    <input type="email" className="email" placeholder="Your Email" />
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder="Your Name" name="your_name" />
+                    <input type="email" className="email" placeholder="Your Email" name="your_email" />
                     <textarea className="msg" name="message" rows={5} placeholder="Your Message"></textarea>
                     <button type="submit" value="Send" className="subBtn">Submit</button>
                     <div className="links">
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
     return (
         <nav className="navbar">
             <img src="https://miro.medium.com/v2/resize:fit:80/format:webp/1*y6C4nSvy2Woe0m7bWEn4BA.png" alt="Logo" className="Logo"></img>
@@ -16,6 +17,14 @@ const Navbar = () => {
                 <img src={require("../../assets/contact.png")} alt="Contact" className="ContactButtonImg"></img>
                 Contact Me
             </button>
+            <img src={require("../../assets/menu.png")} alt="ddMenu" className="ddMenu" onClick={()=>setShowMenu(!showMenu)}></img>
+            <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+                <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Home</Link>
+                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>About</Link>
+                <Link activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+                <Link activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Clients</Link>
+                <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+            </div>
         </nav>
     )
 }

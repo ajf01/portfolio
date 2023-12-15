78af3bf90954c176b44995158698adcbcc03dc7a
ajf01 <ajf010@ucsd.edu>
2023-12-14 17:21:58 -0800

    Fixed styling issues for navbar caused by React

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -7,13 +7,23 @@ const Intro = () => {
     return (
         <section id="intro">
             <div className="introContent">
-                <span className="hello">Hello, </span>
+                <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="myName">Anthony Fong</span> <br/>A Data Scientist</span>
                 <p className="introPara">
                     I specialize in Software and Analytics with a focus on building ML models.
                     <br/>
                     Welcome to my Portfolio Website!
                 </p>
+                {/*<br/>
+                <span className="introText">Main Languages</span>
+                <p className="introPara">
+                    Python, JavaScript, Java
+                </p>
+                <br/>
+                <span className="introText">Interests</span>
+                <p className="introPara">
+                    Financial Literacy, Virtual Reality, Video Games, Boba
+                </p>*/}
                 <Link>
                     <button className="hireBtn" onClick={() => {window.location.href = "#/clients";}}>
                         Hire Me
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import './navbar.css'
 /* import { Link } from 'react-scroll'; */
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false)
     return (
         <nav className="navbar">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/%27%27Logo_AF%27%27.png" alt="Logo" width="65" height="45" className="Logo"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/%27%27Logo_AF%27%27.png" style={{'object-fit': 'cover', 'border-radius': '0.5rem'}} alt="Logo" width="65" height="45" className="Logo"></img>
             <div className="Menu">
-                <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="MenuItem">Home</Link>
-                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">About</Link>
-                <Link activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Portfolio</Link>
-                <Link activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Contact</Link>
+                <NavLink activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="MenuItem">Home</NavLink>
+                <NavLink activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">About</NavLink>
+                <NavLink activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Portfolio</NavLink>
+                <NavLink activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="MenuItem">Contact</NavLink>
             </div>
             {/* <button className="ContactButton" onClick={() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}> */}
             <Link to="clients" spy={true} smooth={true} offset={-70} duration={500} className="ContactButton" >
@@ -21,11 +21,11 @@ const Navbar = () => {
             </Link>
             <img src={require("../../assets/menu.png")} alt="ddMenu" className="ddMenu" onClick={()=>setShowMenu(!showMenu)}></img>
             <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
-                <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Home</Link>
-                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>About</Link>
-                <Link activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
-                <Link activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Clients</Link>
-                <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+                <NavLink activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Home</NavLink>
+                <NavLink activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>About</NavLink>
+                <NavLink activeClass="active" to="exper" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Portfolio</NavLink>
+                <NavLink activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Clients</NavLink>
+                <NavLink activeClass="active" to="clients" spy={true} smooth={true} offset={-70} duration={500} className="navItem" onClick={()=>setShowMenu(false)}>Contact</NavLink>
             </div>
         </nav>
     )
